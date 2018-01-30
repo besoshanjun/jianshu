fa0a432646a12b076e9743f7b504e7ca509daa6c
dcoco_ing@163.com <dcoco_ing@163.com>
2018-01-30 17:56:39 +0800

    分页创建中....

--- pages/index.vue
+++ pages/index.vue
@@ -67,7 +67,117 @@
                                             <nuxt-link to="/u/123" class="nickname">萌薇</nuxt-link>
                                             <span class="time">8小时前</span>
                                         </div>
-
+                                    </div>
+                                    <nuxt-link class="title" to="/p/123">
+                                        如何把书中的知识转化成能力？这是我见过的最好回答
+                                    </nuxt-link>
+                                    <p class="abstract">
+                                        随着知识经济的兴起，读书的重要性得到了大家的普遍认同，于是市场上出现很多读书方法，比如速度、多读、慢读、列读……不管你采用哪一种读书方法，最终都要回归到“行动”二字上，正如陆...
+                                    </p>
+                                    <div class="meta">
+                                        <nuxt-link to="/collection/123" class="collection-tag">
+                                            读书
+                                        </nuxt-link>
+                                        <nuxt-link class="tag" to="p/123">
+                                            <i class="fa fa-eye"></i>
+                                            12054
+                                        </nuxt-link>
+                                        <nuxt-link class="tag" to="/p/123">
+                                            <i class="fa fa-comment"></i>
+                                            96
+                                        </nuxt-link>
+                                        <span>
+                                            <i class="fa fa-heart"></i>
+                                            800
+                                        </span>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="content">
+                                    <div class="author">
+                                        <nuxt-link to="/u/123" class="avatar">
+                                            <img src="../assets/img/user.jpg" alt="">
+                                        </nuxt-link>
+                                        <div class="info">
+                                            <nuxt-link to="/u/123" class="nickname">萌薇</nuxt-link>
+                                            <span class="time">8小时前</span>
+                                        </div>
+                                    </div>
+                                    <nuxt-link class="title" to="/p/123">
+                                        如何把书中的知识转化成能力？这是我见过的最好回答
+                                    </nuxt-link>
+                                    <p class="abstract">
+                                        随着知识经济的兴起，读书的重要性得到了大家的普遍认同，于是市场上出现很多读书方法，比如速度、多读、慢读、列读……不管你采用哪一种读书方法，最终都要回归到“行动”二字上，正如陆...
+                                    </p>
+                                    <div class="meta">
+                                        <nuxt-link to="/collection/123" class="collection-tag">
+                                            读书
+                                        </nuxt-link>
+                                        <nuxt-link class="tag" to="p/123">
+                                            <i class="fa fa-eye"></i>
+                                            12054
+                                        </nuxt-link>
+                                        <nuxt-link class="tag" to="/p/123">
+                                            <i class="fa fa-comment"></i>
+                                            96
+                                        </nuxt-link>
+                                        <span>
+                                            <i class="fa fa-heart"></i>
+                                            800
+                                        </span>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="have-img">
+                                <nuxt-link class="wrap-img" to="/p/123">
+                                    <img src="../assets/img/pic-1.jpg">
+                                </nuxt-link>
+                                <div class="content">
+                                    <div class="author">
+                                        <nuxt-link to="/u/123" class="avatar">
+                                            <img src="../assets/img/user.jpg" alt="">
+                                        </nuxt-link>
+                                        <div class="info">
+                                            <nuxt-link to="/u/123" class="nickname">萌薇</nuxt-link>
+                                            <span class="time">8小时前</span>
+                                        </div>
+                                    </div>
+                                    <nuxt-link class="title" to="/p/123">
+                                        如何把书中的知识转化成能力？这是我见过的最好回答
+                                    </nuxt-link>
+                                    <p class="abstract">
+                                        随着知识经济的兴起，读书的重要性得到了大家的普遍认同，于是市场上出现很多读书方法，比如速度、多读、慢读、列读……不管你采用哪一种读书方法，最终都要回归到“行动”二字上，正如陆...
+                                    </p>
+                                    <div class="meta">
+                                        <nuxt-link to="/collection/123" class="collection-tag">
+                                            读书
+                                        </nuxt-link>
+                                        <nuxt-link class="tag" to="p/123">
+                                            <i class="fa fa-eye"></i>
+                                            12054
+                                        </nuxt-link>
+                                        <nuxt-link class="tag" to="/p/123">
+                                            <i class="fa fa-comment"></i>
+                                            96
+                                        </nuxt-link>
+                                        <span>
+                                            <i class="fa fa-heart"></i>
+                                            800
+                                        </span>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="content">
+                                    <div class="author">
+                                        <nuxt-link to="/u/123" class="avatar">
+                                            <img src="../assets/img/user.jpg" alt="">
+                                        </nuxt-link>
+                                        <div class="info">
+                                            <nuxt-link to="/u/123" class="nickname">萌薇</nuxt-link>
+                                            <span class="time">8小时前</span>
+                                        </div>
                                     </div>
                                     <nuxt-link class="title" to="/p/123">
                                         如何把书中的知识转化成能力？这是我见过的最好回答
@@ -96,6 +206,8 @@
                             </li>
                         </ul>
                     </div>
+                    <!--阅读更多-->
+                    <nuxt-link to="/" class="load-more">阅读更多</nuxt-link>
                 </div>
                 <div class="col-4 aside">
                     <div class="board">
@@ -115,9 +227,95 @@
                             <img src="../assets/img/right-5.png">
                         </nuxt-link>
                     </div>
+                    <div class="download">
+                        <img src="../assets/img/zs.jpg">
+                    </div>
+                    <div class="recommend-author">
+                        <div class="title">
+                            <span>推荐作者</span>
+                            <a href="javascript:void(0)" class="page-change" @click="rotate">
+                                <i ref="refresh" class="fa fa-refresh"></i>
+                                换一批
+                            </a>
+                        </div>
+                        <ul class="list">
+                            <li>
+                                <nuxt-link class="avatar" to="/u/123">
+                                    <img src="../assets/img/user.jpg">
+                                </nuxt-link>
+                                <nuxt-link class="follow" to="/u/123">
+                                    <i class="fa fa-plus-square-o"></i>
+                                    关注
+                                </nuxt-link>
+                                <nuxt-link class="name" to="/u/123">
+                                    占小狼
+                                </nuxt-link>
+                                <p>
+                                    写了152.9k·7.2k喜欢
+                                </p>
+                            </li>
+                            <li>
+                                <nuxt-link class="avatar" to="/u/123">
+                                    <img src="../assets/img/user.jpg">
+                                </nuxt-link>
+                                <nuxt-link class="follow" to="/u/123">
+                                    <i class="fa fa-plus-square-o"></i>
+                                    关注
+                                </nuxt-link>
+                                <nuxt-link class="name" to="/u/123">
+                                    占小狼
+                                </nuxt-link>
+                                <p>
+                                    写了152.9k·7.2k喜欢
+                                </p>
+                            </li>
+                            <li>
+                                <nuxt-link class="avatar" to="/u/123">
+                                    <img src="../assets/img/user.jpg">
+                                </nuxt-link>
+                                <nuxt-link class="follow" to="/u/123">
+                                    <i class="fa fa-plus-square-o"></i>
+                                    关注
+                                </nuxt-link>
+                                <nuxt-link class="name" to="/u/123">
+                                    占小狼
+                                </nuxt-link>
+                                <p>
+                                    写了152.9k·7.2k喜欢
+                                </p>
+                            </li>
+                        </ul>
+                        <nuxt-link to="/" class="find-more">
+                            查看全部
+                            <i class="fa fa-angle-right"></i>
+                        </nuxt-link>
+                    </div>
                 </div>
             </div>
         </div>
+        <footer class="my-container">
+            <div class="row">
+                <div class="col-9">
+                   <ul class="list">
+                       <li>关于简书&nbsp;·&nbsp;</li>
+                       <li>联系我们&nbsp;·&nbsp;</li>
+                       <li>加入我们&nbsp;·&nbsp;</li>
+                       <li>简书出版&nbsp;·&nbsp;</li>
+                       <li>品牌与徽标&nbsp;·&nbsp;</li>
+                       <li>帮助中心&nbsp;·&nbsp;</li>
+                       <li>合作伙伴</li>
+                   </ul>
+                    <div class="jiesao">
+                        ©2012-2018 上海佰集信息科技有限公司 / 简书 / 沪ICP备11018329号-5 / &x3000:;
+                        <img src="../assets/img/sm.png">
+                        沪公网安备31010402002252号 /
+                        <img src="../assets/img/phone.png">
+                        举报电话：021-34770013 /
+                        <img src="../assets/img/zz.png">
+                    </div>
+                </div>
+            </div>
+        </footer>
     </div>
 </template>
 <script>
@@ -148,11 +346,22 @@
                         nextEl: '.swiper-button-next',
                         prevEl: '.swiper-button-prev',
                     }
-                }
+                },
+                deg:0
             }
         },
         components: {
             myHeader
+        },
+        mouted(){
+
+        },
+        methods:{
+            rotate:function () {
+                this.deg = this.deg + 360
+                let deg = this.$refs.refresh.style
+                this.$refs.refresh.style.transform = `rotate(${this.deg}deg)`;
+            }
         }
     }
 </script>
---
+++ pages/p/_id.vue
@@ -0,0 +1,77 @@
+<template>
+    <div>
+        <my-header></my-header>
+        <div class="note">
+            <!--文章区域-->
+            <div class="post">
+                <!--正文-->
+                <div class="article">
+                    <h1 class="title">月薪三千，就该去死吗？</h1>
+                    <div class="author">
+                        <nuxt-link class="avatar" to="/u/123">
+                            <img src="../../assets/img/user.jpg">
+                        </nuxt-link>
+                        <div class="info">
+                            <span class="name">测试用户</span>
+                            <a href="javascript:void(0)" ref="a" class="btn-sm follow" @click="btnPay" @mouseover="over" @mouseleave="out">
+                                <i ref="payi" class="fa fa-plus-square-o"></i>
+                                <span ref="pay">关注</span>
+                            </a>
+                            <div class="meta">
+                                <span>2018.01.23 16:09</span>
+                                <span>字数 2273</span>
+                                <span>阅读 882</span>
+                                <span>评论 7</span>
+                                <span>喜欢 34</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--炫富的操作框-->
+            <div class="side-bar"></div>
+            <!--对应的推荐阅读-->
+            <div class="note-bottom"></div>
+        </div>
+    </div>
+</template>
+<script>
+    import myHeader from '~/components/myHeader'
+    export default{
+        data(){
+            return{
+                pay:true
+            }
+        },
+        components:{
+            myHeader
+        },
+        methods:{
+            btnPay(){
+                if(this.pay){
+                    this.$refs.pay.innerText ='已关注'
+                    this.$refs.payi.className='fa fa-check'
+                    this.$refs.a.className = 'btn-sm notfollow'
+                    this.pay = !this.pay
+                }else{
+                    this.$refs.pay.innerText = '关注'
+                    this.$refs.payi.className='fa fa-plus-square-o'
+                    this.$refs.a.className ='btn-sm follow'
+                    this.pay = !this.pay
+                }
+            },
+            over(){
+                if(!this.pay){
+                    this.$refs.pay.innerText ='取消关注'
+                    this.$refs.payi.className='fa fa-close'
+                }
+            },
+            out(){
+                if(!this.pay){
+                    this.$refs.pay.innerText ='已关注'
+                    this.$refs.payi.className='fa fa-check'
+                }
+            }
+        }
+    }
+</script>
